cf031ea3e088ee9a861b86a2adc58ef3b2232c16
Elle Kaplan <3373930+ejkaplan@users.noreply.github.com>
2026-02-03 11:11:58 -0500

    instructional text

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ from game_world.racetrack import RaceTrack, load_track
 from random_bot import random_move
 import traceback
 
-TRACK = load_track("./tracks/no_choice.pkl")
+TRACK = load_track("./tracks/simple.pkl")
 PLAYER = random_move
 REPLAY_SPEED = 1.0  # seconds per move in the replay. (lower is faster)
 SHOW_REPLAY = True
--- game_world/track_builder.py
+++ game_world/track_builder.py
@@ -6,10 +6,9 @@ import pygame.locals
 from racetrack import RaceTrack, blank_track, load_track
 
 WIDTH = 600
-GRID_SIZE = (5, 5)
-SAVE_FILE_NAME = (
-    "tracks/puzzle.pkl"  # Where do you want to save this track? (Press 'enter' to save)
-)
+GRID_SIZE = (30, 20)
+# Where do you want to save this track? (Press 'enter' to save)
+SAVE_FILE_NAME = "tracks/puzzle.pkl"
 STARTING_TRACK_NAME = None  # None if you want to start blank.
 # Hold A to paint in deactivated walls
 # press up and down on arrow keys to increase brush size
